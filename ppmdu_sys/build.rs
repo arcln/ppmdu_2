4710f9ebf6dc63078667fcd088ee9070699b9f8e
Arthur Chaloin <arthur.chaloin@gmail.com>
2024-10-03 16:11:23 +0200

    fix: rust build script

--- ppmdu_sys/build.rs
+++ ppmdu_sys/build.rs
@@ -1,12 +1,14 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=../ppmdu_audioutil/lib");
-    println!("cargo:rustc-link-arg=-laudioutil");
+    let dir = std::env::var("CARGO_MANIFEST_DIR").expect("missing CARGO_MANIFEST_DIR");
+
+    println!("cargo:rustc-link-search=native={dir}/../ppmdu_audioutil/lib");
+    println!("cargo:rustc-link-lib=audioutil");
     println!("cargo:rustc-link-lib=dylib=c++");
 
-    println!("cargo:rustc-link-search=native=../out/build/arm64-macos-debug/deps/jdksmidi");
-    println!("cargo:rustc-link-arg=-ljdksmidi");
+    println!("cargo:rustc-link-search=native={dir}/../out/build/arm64-macos-debug/deps/jdksmidi");
+    println!("cargo:rustc-link-lib=jdksmidi");
 
-    println!("cargo:rustc-link-search=native=../out/build/arm64-macos-debug/vcpkg_installed/arm64-osx/lib");
-    println!("cargo:rustc-link-arg=-lPocoFoundation");
-    println!("cargo:rustc-link-arg=-lpugixml");
+    println!("cargo:rustc-link-search=native={dir}/../out/build/arm64-macos-debug/vcpkg_installed/arm64-osx/lib");
+    println!("cargo:rustc-link-lib=PocoFoundation");
+    println!("cargo:rustc-link-lib=pugixml");
 }
